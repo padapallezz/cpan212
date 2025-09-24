1d556c4e23858653ebebd9a3cea4714d01e4490d
Trương Nguyễn Phương Anh <padapallezz@Truongs-MacBook-Air.local>
2025-09-24 14:08:26 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
+const port = 3000;
+
 
 app.use(express.json());
 
-//import routes
 
+const invoiceRoutes = require('./routes/invoiceRoutes');
+const expenseRoutes = require('./routes/expenseRoutes');
+const forecastRoutes = require('./routes/forecastRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+app.use('/invoices', invoiceRoutes);
+app.use('/expenses', expenseRoutes);
+app.use('/forecasts', forecastRoutes);
+app.use('/auth', authRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to your BTracker!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
