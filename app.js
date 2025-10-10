b700cd1944998e02505b46c96ead830ce5a36214
Mya05082004 <tkieumy05082004@gmail.com>
2025-10-10 15:56:19 -0400

    Apply Modular Architecture

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//application-level middlewares
+//parse request body
+app.use(express.json()); //parse JSON
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
+//hanlde 404 not found routes
+app.use((req, res) => {
+  res.status(404).json({message: "Route not found"});
+})
 
+//Global error handler, log the error for debugging
+server.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "Something went wrong on the server."})
+})
+
+app
 
 const invoiceRoutes = require('./routes/invoiceRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
